bd73663de55430e855ac7def159cba003913b5f9
Fiyinfoluwa Afolayan <fiyinfoluwaafol@meta.com>
2024-06-14 01:05:41 -0700

    added user feedback for UX - movieCard

--- src/components/ModalView/ModalView.jsx
+++ src/components/ModalView/ModalView.jsx
@@ -11,21 +11,24 @@ function ModalView(props) {
   return (
   <div className={`modal-view ${props.movie? 'show' : 'hide'}`}>
     {/* <div className="modal-view modal-close"> */}
-    <div className="modal-content">
+    <div className="modal-content" style={
+      {backgroundImage: `linear-gradient(
+        rgba(0, 0, 0, 0.8),
+        rgba(0, 0, 0, 0.8)), url(https://image.tmdb.org/t/p/original/${props.movie.backdrop_path})`,
+      backgroundSize: "cover",
+      }
+      }>
       <h2>{props.movie.title}</h2>
-      {/* <h2>Movie Title</h2> */}
-      <img src={`https://image.tmdb.org/t/p/original/${props.movie.backdrop_path}`} alt={props.movie.title}/>
-      {/* <img src="https://placehold.co/600x400" /> */}
-
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" width="560" height="315" src={`https://www.youtube.com/embed/${props.movie.videos.results[0].key}`} allowFullScreen></iframe>
+      </div>
+      {/* <img src={`https://image.tmdb.org/t/p/original/${props.movie.backdrop_path}`} alt={props.movie.title}/> */}
       <p>
-        {/* Release Date: Release Date */}
         Release Date: {props.movie.release_date}
-
       </p>
       <p>
-        {/* Overview: Overview of the movie */}
-        Overview: {props.movie.overview}
-        </p>
+        {props.movie.overview}
+      </p>
       <p>
         Genres: {props.movie.genres.map(genre => genre.name).join(", ")}
       </p>
@@ -33,10 +36,6 @@ function ModalView(props) {
         {/* Runtime: 00h 00mins */}
         Runtime: {runtimeToHrsMins(props.movie.runtime)}
       </p>
-      {/* Embedded Youtube Video */}
-      <div className="embed-responsive embed-responsive-16by9">
-        <iframe className="embed-responsive-item" width="560" height="315" src={`https://www.youtube.com/embed/${props.movie.videos.results[0].key}`} allowFullScreen></iframe>
-      </div>
       <button className="close" onClick={props.onClose}>&times;</button>
       {/* <button className="close" onClick={props.onClose}>&times;</button> */}
     </div>
